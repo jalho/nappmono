207ecfad8ae000e4573cb7c617c567af511e7513
jalho <alho.jere@gmail.com>
2021-01-13 17:23:12 +0200

    Add GraphQL endpoints to UI

--- packages/frontend/src/components/Top.js
+++ packages/frontend/src/components/Top.js
@@ -8,6 +8,23 @@ const Top = () => {
         Name App Monorepo.
         Source on <a href="https://github.com/jalho/nappmono">GitHub</a>.
       </p>
+
+      <p>
+        <span>
+          {"Explore the visual GraphQL API endpoint at "}
+        </span>
+        <a href={process.env.NODE_ENV === "development"
+          ? "http://localhost:4000/graphiql"
+          : "/graphiql" // relative link when in production
+        }>
+          <code>/graphiql</code>
+        </a>
+        <span>.</span>
+        <br/>
+        <span>{"Client sends requests to "}</span>
+        <code>/graphql</code>
+        <span>.</span>
+      </p>
     </div>
   );
 };
